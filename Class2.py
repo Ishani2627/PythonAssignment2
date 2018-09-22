c696b16165517473774f62156c0400965c078e65
IshaniJain <ishanijain27@gmail.com>
2018-09-22 15:23:22 -0700

    second assignment

---
+++ Class2.py
@@ -0,0 +1,30 @@
+#Que1
+print("Hello World!!")
+
+#Que2
+firstname = ("Ishani")
+lastname = ("Jain")
+print("firstname" + " " + "lastname")
+
+#Que3
+x = input("Enter 1st value : ")
+y = input("Enter 2nd value : ")
+z = input("Enter 3rd value : ")
+print(x)
+print(y)
+print(z)
+
+#Que4
+print("Let's get started")
+
+#Que5
+s = "Acadview"
+course = "python"
+fees = 5000
+print(s, course, fees)
+
+#Que6
+pi = 3.14
+r = float(input("Enter the radius :"))
+area = pi*r*r
+print(area)
